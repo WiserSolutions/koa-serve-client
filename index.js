c014f8645251aba64be596f9e36e7fdbd89dfde4
hon2a <jkonopasek@wiser.com>
2018-10-08 11:40:46 +0200

    Simplified serving of static/dist assets (static files are now served at root). Removed `serveStatic` and `serveDist` exports.

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const serve = require('koa-static')
-const mount = require('koa-mount')
 const compose = require('koa-compose')
 const { createReadStream } = require('fs')
 const { resolve } = require('path')
-
-const serveStatic = (staticDir, options) => mount('/static', serve(staticDir, options))
-
-const serveDist = (distDir, options) => serve(distDir, options)
 
 const serveIndex = (indexFile, exclude) => async (ctx, next) => {
   if (exclude(ctx.path)) return await next()
@@ -20,16 +15,14 @@ const serveClient = ({
   staticDir = resolve(rootDir, './static'),
   distDir = resolve(rootDir, './dist'),
   indexFile = resolve(distDir, './index.html'),
-  exclude = path => /^\/(?:static|api)\//.test(path),
+  exclude = path => /^\/api\//.test(path),
   log = console.log.bind(console) // eslint-disable-line no-console
 } = {}) => {
   log && log(`Serving /static, /dist, and index in ${rootDir}`)
-  return compose([serveStatic(staticDir), serveDist(distDir), serveIndex(indexFile, exclude)])
+  return compose([serve(staticDir), serve(distDir), serveIndex(indexFile, exclude)])
 }
 
 module.exports = {
-  serveStatic,
-  serveDist,
   serveIndex,
   serveClient
 }
